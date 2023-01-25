1d8d28ee2472b277c83eb290a9d5aeb5a63fd947
Becca <rkinnee91@hotmail.com>
2023-01-24 16:56:05 -0800

    add fonts. Finalize typography

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,7 +2,10 @@ import './NavBar.scss'
 
 function NavBar() {
     return(
-        <></>
+        <section className="navbar">
+            <h1 className="navbar__header">Rebecca Kinnee</h1>
+            <h4 className="navbar__subHeader">Front End Web Developer</h4>
+        </section>
     )
 }
 
